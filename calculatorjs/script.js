23161902b20b32226f88595829766f535cc252b9
Mark <markmwago@gmail.com>
2019-02-12 16:29:39 +0300

    add functions

---
+++ calculatorjs/script.js
@@ -0,0 +1,16 @@
+var add = function(number1, number2){
+return number1+number2
+};
+alert(add(6,7));
+
+var subtract = function(number1, number2){
+return number1-number2
+};
+alert(subtract(10,5));
+
+var add = function(number1, number2){
+return number1+number2
+};
+var number1 = prompt("Enter a number")
+var number2 = prompt("Enter another number");
+alert(add(4,3));
